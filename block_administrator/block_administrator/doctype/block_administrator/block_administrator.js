54ea14013bf7341abd7a31d9335ef5f2cce2bdf0
Immanuel Raj <iamimmanuelraj@gmail.com>
2025-03-31 18:23:19 +0000

    feat: Add Block Administrator DocType with initial implementation
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

---
+++ block_administrator/block_administrator/doctype/block_administrator/block_administrator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Immanuel Raj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Block Administrator", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ block_administrator/block_administrator/doctype/block_administrator/block_administrator.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Immanuel Raj and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BlockAdministrator(Document):
+	pass
