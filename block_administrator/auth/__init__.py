7afacb51adb90998ab8820d79576677817412b27
Immanuel Raj <iamimmanuelraj@gmail.com>
2025-03-31 20:47:37 +0000

    feat: Enhance administrator login validation with configurable block option
    Signed-off-by: Immanuel Raj <iamimmanuelraj@gmail.com>

--- block_administrator/auth/__init__.py
+++ block_administrator/auth/__init__.py
@@ -3,7 +3,9 @@ import frappe
 
 def validate():
     if frappe.session.user == "Administrator":
-        frappe.throw(
-            msg=frappe._("Administrator login is disabled"),
-            exc=frappe.PermissionError,
-        )
+        block_admin = frappe.get_single("Block Administrator")
+        if block_admin.block_administrator_login == 1:
+            frappe.throw(
+                msg=frappe._("Administrator login is disabled"),
+                exc=frappe.PermissionError,
+            )
