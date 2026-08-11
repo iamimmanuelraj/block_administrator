f3ed9400b4d72881569cbd940752f951ee80a293
Immanuel Raj <iamimmanuelraj@gmail.com>
2026-08-12 01:10:58 +0530

    fix: align license metadata with GPLv3 LICENSE file (#29)

--- block_administrator/hooks.py
+++ block_administrator/hooks.py
@@ -3,7 +3,7 @@ app_title = "Block Administrator"
 app_publisher = "Immanuel Raj"
 app_description = "App to block login of frappe administrator account when email-pass login method is enabled"
 app_email = "iamimmanuelraj@gmail.com"
-app_license = "mit"
+app_license = "gpl-3.0"
 
 # Apps
 # ------------------
